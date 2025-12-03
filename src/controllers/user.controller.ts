5b823591cafe8f28dafc79e2a3cb03b726a82bc2
Korolchuk-Olha <olkakorolcuk@gmail.com>
2025-12-04 00:28:10 +0200

    GA-5: Added api for changing use data and rewrite get details

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,11 @@
 import { generateAccessToken, generateRefreshToken } from "@/config/jwt";
 import UserEntity from "@/entities/User.entity";
+import { IPlan } from "@/types/entities";
 import { EPlan, EResponseMessage, EStatus } from "@/types/enums";
 import bcrypt from "bcryptjs";
 import { NextFunction, Request, Response } from "express";
-import { v4 as uuidv4 } from "uuid";
 import jwt from "jsonwebtoken";
-import { IPlan } from "@/types/entities";
+import { v4 as uuidv4 } from "uuid";
 
 export const register = async (
   req: Request,
